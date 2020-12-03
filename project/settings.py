f7bf4f528716eda433b94b52f08988be613fdeaa
Ahmed Barhoma <63800760+AhmedStorm@users.noreply.github.com>
2020-12-04 00:21:16 +0200

    adding job fields DB

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'job',
+    'home',
+    'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
