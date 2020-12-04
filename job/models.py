6e8a5c5f075436e576f293a8c26fe77439a95239
Ahmed Barhoma <63800760+AhmedStorm@users.noreply.github.com>
2020-12-04 03:21:09 +0200

    add url

--- job/models.py
+++ job/models.py
@@ -9,14 +9,19 @@ JOB_TYPE= (
 class Job(models.Model):
     title = models.CharField(max_length=100)
     #Location
-   
+
     job_type = models.CharField(max_length=100,choices=JOB_TYPE)
     description = models.TextField(max_length=1000)
     published_at = models.DateTimeField(auto_now=True)
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
-
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
     def __str__(self):
         return self.title
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+    def __str__(self):
+        return self.name
